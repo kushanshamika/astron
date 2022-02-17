5b854658f29af75883e66a994502ffa2a9857d0e
Kushan Shamika <shamikakushan@gmail.com>
2022-02-17 23:02:45 +0530

    🔧 JSONRemove

--- lib/bff/request/fetch.ts
+++ lib/bff/request/fetch.ts
@@ -3,12 +3,10 @@ import axios from 'axios';
 export async function JSONFetch(actionObj, bffResponse) {
   const response = await axios.get(actionObj['url']);
 
-  if (actionObj['action'] === 'merge') {
-    if (actionObj['key']) {
-      bffResponse[actionObj['key']] = response.data;
-    } else {
-      bffResponse = { ...bffResponse, ...response.data };
-    }
+  if (actionObj['key']) {
+    bffResponse[actionObj['key']] = response.data;
+  } else {
+    bffResponse = { ...bffResponse, ...response.data };
   }
 
   return bffResponse;
