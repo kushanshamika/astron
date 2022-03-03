bc8a8a52e57f0ab8dd8a1251fc5fe87017f9fa9b
Kushan Shamika <shamikakushan@gmail.com>
2022-03-03 22:53:47 +0530

    ✨ Attaching Query Parameters #2

--- lib/bff/request/fetch.ts
+++ lib/bff/request/fetch.ts
@@ -1,7 +1,13 @@
 import axios from 'axios';
+import { queryParams } from '../../policies/request-parameters';
+import { parameterReplace } from '../../policies/request-parameters/url-parameters';
 
-export async function JSONFetch(actionObj, bffResponse) {
-  const response = await axios.get(actionObj['url']);
+export async function JSONFetch(actionObj, bffResponse, req) {
+  console.log(parameterReplace(actionObj['url'], actionObj['params'], req, bffResponse));
+
+  const response = await axios.get(await parameterReplace(actionObj['url'], actionObj['params'], req, bffResponse), {
+    params: await queryParams(actionObj['query'], req, bffResponse),
+  });
 
   if (actionObj['key']) {
     bffResponse[actionObj['key']] = response.data;
