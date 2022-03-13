04cb060434dabc589bccebc6eb94596a56b30f8b
Kushan Shamika <shamikakushan@gmail.com>
2022-03-13 15:53:44 +0530

    ➕ cors

--- lib/index.ts
+++ lib/index.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import * as config from 'config';
 import * as bodyParser from 'body-parser';
 import * as mongoose from 'mongoose';
+import * as cors from 'cors';
 
 import { proxy } from './proxy';
 import endpointRoute from '../admin/endpoints/endpointRoutes';
@@ -13,6 +14,7 @@ mongoose.connect('mongodb://astron:astron@localhost:27017/astron');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 endpointRoute(app);
 
